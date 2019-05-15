67f576eb198aa15accb9fca4191dbf99ee3c30ca
ali4728 <erkan.ali.yilmaz@gmail.com>
2019-05-14 23:22:44 -0700

    Powershel for sql tutorials

---
+++ Untitled2.ps1
@@ -0,0 +1,33 @@
+﻿Clear-Host
+#hash
+$hash = @{}
+$hash.Add("name","erkan")
+
+$hash.Add("job","developer")
+$hash.Add("Age",45)
+
+# update value at certain key
+#$hash["name"] = "Pelin"
+
+Write-Output ""
+Write-Output ""
+
+#get value for certain keys
+#$hash["name"]
+#$hash.GetType()
+
+#$hash.Count
+Write-Output ""
+Write-Output "Hash Values are filtered"
+$hash.Values | where {$_ -match "e"}
+Write-Output ""
+Write-Output ""
+$hash.Keys
+
+$ary = @(1,2,3,4,5,6,7,8,9) 
+$bb = $ary.GetEnumerator() | where {$_ -gt 5}
+Write-Output "will print bb"
+$bb
+$bb.GetType()
+
+
